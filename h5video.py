c31dd490d948f9820ef40127ff5241d3b7e3a61f
markhannel <mdh386@nyu.edu>
2017-04-21 11:53:43 -0400

    Added several features to h5video including attributes for the number of frames and the size of an image. The example code was improved.

--- h5video.py
+++ h5video.py
@@ -7,26 +7,33 @@ class h5video(object):
         self.mode = mode
         self.image = None
         self.index = None
-        self.keys = self.f['images/'].keys()
-        self.rewind()
+        self.dim = None
+        self.nframes = None
 
     def __enter__(self):
         self.f = h5py.File(self.filename, self.mode)
-        return self.f
+        self.keys = self.f['images/'].keys()
+        self.nframes = len(self.keys)
+        self.image = self.rewind()
+        self.dim = self.f['images/' + self.keys[0]].shape
+        return self
 
     def __exit__(self, *args):
         self.f.close()
 
-    def view_image(self):
+    def get_image(self):
         try:
-            self.image = self.f['images/' + self.keys[index]]
+            self.image = self.f['images/' + self.keys[self.index]]
             return self.image
         except KeyError:
             print 'Indexed frame is not present.'
+            
+    def get_time(self):
+        return self.keys[self.index]
 
     def rewind(self):
         self.index = 0
-        return self.view_image()
+        return self.get_image()
 
     def next(self):
         self.index += 1
@@ -34,22 +41,29 @@ class h5video(object):
 
     def goto(self, index):
         self.index = index
-        
 
 def example():
     import matplotlib.pyplot as plt
 
     # Grab h5 file.
-    filename = 'x_00_00.h5'
-    
+    filename = 'example.h5'
+    bg = np.load('example_bg.npy')
+
     with h5video(filename) as vid:
-        plt.imshow(vid.view_image)
+        plt.imshow(vid.get_image()/bg)
+        plt.gray()
         plt.show()
 
-        vid.goto(100)
+        vid.goto(220)
         
-        plt.imshow(vid.view_image)
+        plt.imshow(vid.get_image()/bg)
+        plt.gray()
         plt.show()
+        
+        print('Example timestamp: {}'.format(vid.get_time()))
+        print('Dimension of image: {}'.format(vid.dim))
+        print('Number of frames: {}'.format(vid.nframes))
+    
     
 if __name__ == '__main__':
     example()
